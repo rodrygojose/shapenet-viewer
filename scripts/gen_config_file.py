d61323627baf28bbfc57e08bb0783279f6b83074
Rodrigo Ortiz Cayon <rodrigo.ortiz-cayon@inria.fr>
2016-04-26 09:54:28 +0200

    add config file

--- scripts/gen_config_file.py
+++ scripts/gen_config_file.py
@@ -1,17 +1,18 @@
 # Gen configuration file for render all models of a directory with shapeNet viewer
 
-from os import listdir
-from os import path
 import os 
 
-DATABASE_PATH = "/user/rortizca/home/workspace/useful_code/shapenet-viewer/scripts"
-CONFIG_FILE = "configFile.txt"
+DATABASE_PATH = "D:/D_workspace/database/shapenet-models"
+CONFIG_FILE   = "configFile.txt"
 
-dirs = filter(os.path.isdir, os.listdir(DATABASE_PATH)) 
+dirs = os.listdir( DATABASE_PATH )
+
 file = open( CONFIG_FILE,'w')
+file.write("viewer.commands = [");
 
 for dir in dirs:
-  file.write( dir + ' n')
+  file.write("\"load " + dir + "\", \"save model screenshots\", ")
 
+file.write("]");
 file.close()
 
